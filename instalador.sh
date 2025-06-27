ac2f9242188e256b97a5149bf3894daab0cd0fa8
Arahnose21x <rastupropuits@gmail.com>
2025-06-27 00:07:03 -0400

    Create instalador.sh

---
+++ instalador.sh
@@ -0,0 +1,15 @@
+#Instalar git
+pkg install git
+#instalar python
+pkg install python
+#instalar micro
+pkg install micro
+
+echo "
+Se instalo:
+
+-Git (Nesesario para copiar la repo)
+-Python (Si es que lo requieres)
+-Micro (Para más comodidad)
+
+git clone https://github.com/Arahnose21x/Termux_essentials.git && cd termux_essentials && bash Utilidades.sh
